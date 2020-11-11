4325946f1ce8c7fd26a9d11625414d9732f1ee4b
Bilal Chehab <bichehab@lixar.com>
2020-11-11 17:40:28 +0200

    remove comments

--- AssemblyHasher/Program.cs
+++ AssemblyHasher/Program.cs
@@ -8,16 +8,6 @@ namespace AssemblyHasher
     {
         static void Main(string[] args)
         {
-            //var fileToHash1 = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "FileHasher.dll");
-            //var fileToHash2 = Assembly.GetExecutingAssembly().Location;
-            //var fileToHash3 = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"..\..\Program.cs");
-            //var fileToHash4 = @"C:\Users\bchehab\Desktop\AssemblyHasher\ClassLibrary1\bin\Debug\ClassLibrary1.dll";
-
-            //var hash1 = new FileHasher.HashCalculator(fileToHash1).CalculateFileHash();
-            //var hash2 = new FileHasher.HashCalculator(fileToHash2).CalculateFileHash();
-            //var hash3 = new FileHasher.HashCalculator(fileToHash3).CalculateFileHash();
-            //var hash4 = new FileHasher.HashCalculator(fileToHash4).CalculateFileHash();
-
             var fileToHash = args != null && args.Length > 0 ? args[0] : null;
             if (!string.IsNullOrEmpty(fileToHash))
             {
